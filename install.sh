e7a40cf683c4f61721b3c22deb9114acfb4c60b9
Lee Staples <lee@projectvault.co>
2021-12-30 15:35:19 +0000

    Fix subs

--- install.sh
+++ install.sh
@@ -5,21 +5,21 @@ echo "             cloning zsh-autosuggestions                   "
 echo "-----------------------------------------------------------"
 git clone --depth=1 \
     https://github.com/zsh-users/zsh-autosuggestions \
-    "${HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+    "${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 
 echo "==========================================================="
 echo "             cloning zsh-syntax-highlighting               "
 echo "-----------------------------------------------------------"
 git clone --depth=1 \
     https://github.com/zsh-users/zsh-syntax-highlighting.git \
-    "${HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    "${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 
 echo "==========================================================="
 echo "             cloning powerlevel10k                         "
 echo "-----------------------------------------------------------"
 git clone --depth=1 \
     https://github.com/romkatv/powerlevel10k.git \
-    "${HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    "${HOME}/.oh-my-zsh/custom/themes/powerlevel10k"
 
 echo "==========================================================="
 echo "             import zshrc                                  "
